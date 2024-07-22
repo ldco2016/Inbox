c25b271f05e5d16f68c8991e45734cbe3174620e
ldco2016 <renaissance.scholar2012@gmail.com>
2024-07-22 13:35:32 -0500

    Breaking Change - Web3 v4 and Ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,11 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
 
 const provider = ganache.provider();
 const web3 = new Web3(provider);
 
-const { interface, bytecode } = require('../compile');
+const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
@@ -18,19 +18,19 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ['hi there']
+      arguments: ["hi there"],
     })
-    .send({ from: accounts[0], gas: '1000000' });
+    .send({ from: accounts[0], gas: "1000000" });
 
   inbox.setProvider(provider);
 });
 
-describe('inbox', () => {
-  it('deploys a contract', () => {
+describe("inbox", () => {
+  it("deploys a contract", () => {
     assert.ok(inbox.options.address);
   });
-  it('has a default message', async () => {
+  it("has a default message", async () => {
     const message = await inbox.methods.message().call();
-    assert.equal(message, 'hi there');
+    assert.equal(message, "hi there");
   });
 });
